1c6b462fb81510f589ccbf283d08b7117277c6fb
suwandre <suwandresukijat@gmail.com>
2023-01-03 22:57:40 +0100

    added new routes to account.js

--- routes/rh-backend/account.js
+++ routes/rh-backend/account.js
@@ -4,6 +4,12 @@ const {
     removeLoggedInUser,
     retrieveUserBySessionToken,
     userLogin,
+    addUserData,
+    addPlayfabId,
+    getUserIdFromUniqueHash,
+    getPlayfabId,
+    checkPlayfabIdExists,
+    checkUserDataExists,
 } = require('../../api/rh-backend/account');
 const router = express.Router();
 
@@ -45,6 +51,72 @@ router.post('/userLogin', async (req, res) => {
 
     try {
         const result = await userLogin(username, password);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.post('/addUserData', async (req, res) => {
+    const { userObjId, playfabId } = req.body;
+
+    try {
+        const result = await addUserData(userObjId, playfabId);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.post('/addPlayfabId', async (req, res) => {
+    const { userObjId, playfabId } = req.body;
+
+    try {
+        const result = await addPlayfabId(userObjId, playfabId);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.post('/getUserIdFromUniqueHash', async (req, res) => {
+    const { uniqueHash } = req.body;
+
+    try {
+        const result = await getUserIdFromUniqueHash(uniqueHash);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.get('/getPlayfabId/:address', async (req, res) => {
+    const address = req.params.address;
+
+    try {
+        const result = await getPlayfabId(address);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.post('/checkPlayfabIdExists', async (req, res) => {
+    const { userObjId } = req.body;
+
+    try {
+        const result = await checkPlayfabIdExists(userObjId);
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ status: 'error', message: err.message });
+    }
+});
+
+router.post('/checkUserDataExists', async (req, res) => {
+    const { userObjId } = req.body;
+
+    try {
+        const result = await checkUserDataExists(userObjId);
         res.json(result);
     } catch (err) {
         res.status(400).json({ status: 'error', message: err.message });
